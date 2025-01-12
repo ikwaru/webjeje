81b811823b1dc547ccf9a884173e33cd478adb7c
anaisgn <anaisgarrofe@gmail.com>
2025-01-12 19:59:06 +0100

    webje

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,6 @@
-/* const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-}) */
-
 // vue.config.js file to be place in the root of your repository
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' 
-  ? '/webjeje/' 
-  : '/'             //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
+	publicPath: process.env.NODE_ENV === 'production' ?
+	'/webjeje-2/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
 }
 
