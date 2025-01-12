325c1530a096bce63c7dc407b652ffaca1246ec3
anaisgn <anaisgarrofe@gmail.com>
2025-01-12 20:37:56 +0100

    o6kiujyrntheybgrewvfac

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 // vue.config.js file to be place in the root of your repository
 module.exports = {
 	publicPath: process.env.NODE_ENV === 'production' ?
-	'/webjeje/webjeje-2/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
+	'/webjeje-2/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
 }
 
