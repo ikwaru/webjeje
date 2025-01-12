3619c169f1c369e984f35f2578ab0b20e2b316fd
anaisgn <anaisgarrofe@gmail.com>
2025-01-12 20:46:31 +0100

    vgtf b h

--- src/router/index.js
+++ src/router/index.js
@@ -45,7 +45,7 @@ const routes = [
 
 const router = new VueRouter({
   mode: "history",
-  base: '/webjeje-2/',
+  base: '/webjeje/',
   routes
 })
 
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 // vue.config.js file to be place in the root of your repository
 module.exports = {
-	publicPath: process.env.NODE_ENV === 'production' ?
-	'/webjeje-2/' : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
+	publicPath: process.env.NODE_ENV === 'production' 
+  ? '/webjeje/' 
+  : '/' //Utilizo el nombre de LA CARPETA DE EJEMPLO. TU USA EL TUYO
 }
 
